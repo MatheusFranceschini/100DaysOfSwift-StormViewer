f870fcb7fb8fb00737ef8c8757d408c72a6d3941
MatheusFranceschini <masuzena@gmail.com>
2024-08-12 22:46:00 -0300

    First images + Designing the interface

--- StormViewer/ViewController.swift
+++ StormViewer/ViewController.swift
@@ -8,13 +8,66 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    private lazy var pictureTableView: UITableView = {
+       let tableView = UITableView()
+        tableView.translatesAutoresizingMaskIntoConstraints = false
+        tableView.dataSource = self
+        tableView.register(UITableViewCell.self, forCellReuseIdentifier: "Picture")
+        return tableView
+    }()
+    
+    var pictures = [String]()
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
-        view.backgroundColor = .red
+        setupFileManager()
+        setupLayout()
+        setupNavigationBar()
     }
 
+    private func setupFileManager() {
+        let fileManager = FileManager.default
+        let path = Bundle.main.resourcePath!
+        let items = try! fileManager.contentsOfDirectory(atPath: path)
+        
+        for item in items {
+            if item.hasPrefix("img") {
+                pictures.append(item)
+            }
+        }
+        
+        print(pictures)
+    }
+    
+    func setupLayout() {
+        view.backgroundColor = .systemBackground
+        
+        view.addSubview(pictureTableView)
+        
+        NSLayoutConstraint.activate([
+            pictureTableView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor),
+            pictureTableView.bottomAnchor.constraint(equalTo: view.bottomAnchor),
+            pictureTableView.trailingAnchor.constraint(equalTo: view.trailingAnchor),
+            pictureTableView.leadingAnchor.constraint(equalTo: view.leadingAnchor)
+        ])
+    }
+    
+    func setupNavigationBar() {
+        navigationItem.title = "Storm Viewer"
+    }
 
 }
 
+extension ViewController: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Picture", for: indexPath)
+        cell.textLabel?.text = pictures[indexPath.row]
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return pictures.count
+    }
+}
